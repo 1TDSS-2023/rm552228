94c93d3e16380322b2dc5cb339dfbe3bdf78cf34
Vittutexx <vitortex06@gmail.com>
2023-05-03 20:52:23 -0300

    defer/.addEventListener/getById/Math/Interpolação

--- js/script.js
+++ js/script.js
@@ -1,21 +1,56 @@
 "use strict";
-//console.log("Olá Mundo!")
+// console.log("Olá Mundo!!");
 
-//var nr1 = "10";
-//var nr2 = 5;
+// var nr1 = "10";
+// var nr2 = "5";
 
-//Realizado operações com Strings;
-//console.log(parseInt(nr1)+parseInt(nr2));
-var nome1 = "Dubas";
-let nome2 = "Serbatinho";
-const nome3 = "Julia";
+// //Realizando operações com Strings e realizando o parse com a
+// //função parseInt...;
+// console.log(parseInt(nr1)+parseInt(nr2));
 
-if(nome1 != ""){
-    let nome1 = "Julia";
+// var nome1 = "Dubas";
+// // let nome2 = "Serbatinho";
+// // const nome3 ="Julia";
+// nome = "Ale";
+// console.log(nome);
 
-}
+// if(nome1 != ""){
+//     let nome1 = "Julia";
+// }
+
+// var nome
+
+// console.log(nome1);
+
+/* <button value="click" id="meu-btn">Click</button> */
+const btn = document.getElementById("meu-btn");
+
+//PRIMEIRO PASSO PARA A TRANSFORMAÇÃO DO BOTÃO EM RGB
+btn.addEventListener("EVENTO", function(){
+   // console.log(btn.textContent);
+    //Função matematica Math
+    //random = Retorna um numero aleatório entre 0 e 1. Ex: 0.2323454545;
+    //floor = Arredonda o numero para baixo.
+    //ceil = Arredonda o numero para cima.
+    //round = Arredonda o numero aleatório.
 
 
 
-console.log(nome1);
+
+
+    let r=0, g=0, b=0;
+
+    r = Math.round(Math.random()* 255);
+    g = Math.round(Math.random()* 255);
+    b = Math.round(Math.random()* 255);
+    
+    this.setAttribute("style",`background-color:rgb(${r},${g},${b});`);
+
+
+
+
+
+
+} );
+
 
